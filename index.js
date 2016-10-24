f5b6f32dd9da36a2f14e3bd45bce4a3511358dc8
Pavkout <pkoutoglou@gmail.com>
2016-10-24 21:24:56 +0300

    Create index.js
    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReFlex from './src/components/reflex.jsx';
+
+ReactDOM.render(<ReFlex initialValue="Reflex" />, document.getElementById('app'));
